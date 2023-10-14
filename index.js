8bae11f4ce4015bde8540f5ca8c4997a2dfcc1e4
michaelbedard <courriermichaelbedard@gmail.com>
2023-10-14 17:02:29 -0400

    change gitignore

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 import express from "express"
+import puppeteer from "puppeteer";
 
 const app = express()
 
-app.listen(8000, () => console.log("App running on port 8000"))
-
 app.get('/', (req, res) => {
     res.json('API running :)')
-})
+});
+
+(async () => {
+    try {
+        console.log("Starting browser")
+        const browser = await puppeteer.launch({
+            headless: true, // Change this to true for headless mode
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--disable-gpu'
+            ],
+        });
+
+        console.log("browser started")
+        const page = await browser.newPage();
+        await page.goto('https://google.com/')
+
+        await page.screenshot({path: "photo.png"})
+        console.log("finished")
+
+        await browser.close(); // Close the browser when you're done
+
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+
+
+
+
+
+app.listen(8000, () => console.log("App running on port 8000"))
