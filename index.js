37082d742e1affc5712aec598480bb89ad8df871
michaelbedard <courriermichaelbedard@gmail.com>
2023-10-14 20:57:53 -0400

    added 2 endpoint for test

--- index.js
+++ index.js
@@ -3,40 +3,56 @@ import puppeteer from "puppeteer";
 
 const app = express()
 
-app.get('/', (req, res) => {
+let temperature = null
+
+app.get('/test', (req, res) => {
     res.json('API running :)')
 });
 
-(async () => {
-    try {
-        console.log("Starting browser")
-        const browser = await puppeteer.launch({
-            headless: true, // Change this to true for headless mode
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-accelerated-2d-canvas',
-                '--no-first-run',
-                '--no-zygote',
-                '--disable-gpu'
-            ],
-        });
+app.get('/temp', (req, res) => {
+    (async () => {
+        try {
+            console.log("Starting browser")
+            const browser = await puppeteer.launch({
+                headless: true, // Change this to true for headless mode
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--disable-gpu'
+                ],
+            });
 
-        console.log("browser started")
-        const page = await browser.newPage();
-        await page.goto('https://google.com/')
+            console.log("browser started")
+            const page = await browser.newPage();
+            await page.goto('https://www.meteomedia.com/ca/meteo/quebec/montreal')
 
-        await page.screenshot({path: "photo.png"})
-        console.log("finished")
+            await page.waitForSelector('#obs_data');
 
-        await browser.close(); // Close the browser when you're done
+            // Get the text content of the element
+            const elementText = await page.evaluate(() => {
+                const element = document.getElementById('obs_data')
+                const temp = element.getElementsByClassName("temp")[0];
+                return temp ? temp.textContent : null;
+            });
 
-    } catch (error) {
-        console.error(error);
-    }
-})();
+            console.log("Text from element:", elementText);
+            temperature = elementText;
 
+            console.log("finished")
+
+            await browser.close(); // Close the browser when you're done
+
+        } catch (error) {
+            console.error(error);
+        }
+        res.json(temperature)
+    })();
+
+});
 
 
 
